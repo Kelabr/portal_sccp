1b8c8f5d3b08264c3691c5afb6bdef57b00031d0
Kel_Seduc <kelabrantes.me@gmail.com>
2025-01-29 15:38:00 -0300

    alinhando cards

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Tutoriais from "@/components/tutoriais/Tutoriais";
 export default function Home() {
   return (
      <main className="pt-20">
-        <section className="px-3">
+        <section className="px-3 flex flex-col items-center">
           <Tutoriais/>
         </section>
     </main>
