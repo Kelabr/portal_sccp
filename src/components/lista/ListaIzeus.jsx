57e55a4a8ed4ff124646872f27ddd577436f1c18
Kel_Seduc <kelabrantes.me@gmail.com>
2025-01-30 15:31:51 -0300

    ajuste responsividade

--- src/components/lista/ListaIzeus.jsx
+++ src/components/lista/ListaIzeus.jsx
@@ -6,7 +6,7 @@ export default function Lista(){
     return(
         <div className="flex w-max-[1200px] flex-col sm:flex-row ">
         <div className=" max-w-[1200px] sm:flex sm:flex-wrap">
-                <Link href='./izeus/cadastro' className="" >
+                <Link href='./izeus/faixa' className="" >
                     <Card className='h-[180px] max-w-[350px] sm:mr-3 sm:mb-3 sm:mt-4'>
                         <CardHeader>
                             <div className="flex mb-2">
@@ -18,7 +18,7 @@ export default function Lista(){
                     </Card>
                 </Link>
 
-                <Link href='./izeus/cadastro' className="" >
+                <Link href='./izeus/req_extra' className="" >
                     <Card className='h-[180px] max-w-[350px] sm:mr-3 mt-4 sm:mt-4 mb-3 sm:mb-0'>
                         <CardHeader>
                             <div className="flex mb-2">
@@ -30,7 +30,7 @@ export default function Lista(){
                     </Card>
                 </Link>
 
-                <Link href='./izeus/cadastro' className="" >
+                <Link href='./izeus/req_eventual' className="" >
                     <Card className='h-[180px] max-w-[350px] sm:mr-3 sm:mt-4'>
                         <CardHeader>
                             <div className="flex mb-2">
